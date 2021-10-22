7e46da06735a083495bf67d2d4a6a244445026e5
sakilk130 <sakilk130@gmail.com>
2021-10-23 00:34:24 +0600

    complete sidebar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './styles/App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello React</h1>
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
