e6538ca6556f99674cd1f1c4868e198e453f0d99
sakilk130 <sakilk130@gmail.com>
2021-10-23 23:01:57 +0600

    complete widgets component

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from 'react-twitter-embed';
+import SearchIcon from '@material-ui/icons/Search';
 
 import '../styles/Widgets.css';
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={'1402320383932502021'} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="sakilk130"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={'https://facebook.com/sakilk130'}
+          options={{ text: '#reactjs is awesome', via: 'sakilk130' }}
+        />
+      </div>
     </div>
   );
 }
